95ee8c1fac02081a26572843b3c03d052a7a12db
Christian Bannes <cbannes@gmail.com>
2017-01-10 21:30:45 +0100

    implement keywords

--- src/main/scala/jerman/JermanTest.scala
+++ src/main/scala/jerman/JermanTest.scala
@@ -4,11 +4,11 @@ import scala.io.Source
 
 
 object JermanTest extends App with KeywordMapper with Arm{
-  val is = getClass().getResourceAsStream("Beispiel.jerman")
+  val is = getClass().getResourceAsStream("/Beispiel.jerman")
 
-  val res = using(Source.fromInputStream(is)) { source =>
-    source.getLines().map(replaceLine(_) + "\n").mkString
-  }
+  val source = Source.fromInputStream(is)
+  val res =  source.getLines().map(replaceLine(_) + "\n").mkString
+
 
   println(res);
 }
--- src/main/scala/jerman/KeywordMapper.scala
+++ src/main/scala/jerman/KeywordMapper.scala
@@ -4,14 +4,28 @@ package jerman
   * Created by cbannes on 29.12.16.
   */
 trait KeywordMapper {
-  val adjectiveSuffix = "(e|(es)){0,1}\\s"
+  val adjectiveSuffix = "(e|(er)|(es)){0,1}\\s"
   val typeSuffix = "(?=(\\s|\\[\\]|\\.\\.\\.))"
+  val obrace = "(\\s)*($|\\{)"
+
   val keywordsMap = Map(
     "Packet(?=\\s)" -> "package",
     "gibzurück(?=\\s)" -> "return",
     "Klasse(?=[{,\\s])" -> "class",
+    "Schnittstelle(?=[{,\\s])" -> "interface",
     "dies." -> "this.",
     "System.aus.druckzl\\(" -> "System.out.println(",
+    "fortsetzen" -> "continue",
+    "für(?=(\\s)*(\\())" -> "for",
+    "schalte(?=(\\s)*(\\())" -> "switch",
+    "wenn(?=(\\s)*(\\())" -> "if",
+    s"dann(?=$obrace)" -> "else",
+    " implementiert(?=\\s)" -> " implements",
+    " erweitert(?=\\s)" -> " extends",
+    " instanzvon(?=\\s)" -> " instanceof",
+    " Fall(?=\\s)" -> " case",
+
+
 
     //types
     s"Zeichenkette$typeSuffix" -> "String",
@@ -21,13 +35,20 @@ trait KeywordMapper {
     s"Zeichen$typeSuffix" -> "char",
     s"Lang$typeSuffix" -> "long",
     s"Leere$typeSuffix" -> "void",
+    s"Bool$typeSuffix" -> "boolean",
+    s"Byte$typeSuffix" -> "byte",
+    s"Kurz$typeSuffix" -> "short",
 
 
     //adjectives
+    s"sychnronisiert$adjectiveSuffix" -> "synchronized ",
+    s"flüchtig$adjectiveSuffix" -> "volatile ",
+    s"vorübergehend$adjectiveSuffix" -> "transient ",
+    s"abstrakt$adjectiveSuffix" -> "abstract ",
     s"öffentlich$adjectiveSuffix" -> "public ",
     s"statisch$adjectiveSuffix" -> "static ",
     s"final$adjectiveSuffix" -> "final ",
-    s"geschützt$adjectiveSuffix" -> "protected",
+    s"geschützt$adjectiveSuffix" -> "protected ",
     s"neu$adjectiveSuffix" -> "new ",
     "(?<=\\s)haupt(?=[(,\\s])" -> "main");
 
@@ -37,3 +58,16 @@ trait KeywordMapper {
   def replaceKeywords(line: String): String =
     keywordsMap.foldLeft(line) { case (line, (k,v)) => line.replaceAll(k,v) }
 }
+
+/*
+
+assert***	default	goto*		synchronized
+	do	if	private	this
+break		implements	protected	throw
+	else	import	public	throws
+case	enum****	instanceof	return	transient
+catch	extends		try
+		interface
+	finally		strictfp**	volatile
+const*		native	super	while
+*/
